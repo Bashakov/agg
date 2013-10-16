3e1256c6e0b203b43d0c2bb00bdd1b9d42ba25f2
Andrey <bashakov@mail.ru>
2013-10-16 13:10:23 +0400

    - move svg_parse and draw code into lib - add expat into lib

---
+++ agg-2.4/examples/svg_viewer/expat/expat_configure.h
@@ -0,0 +1,11 @@
+#ifndef EXPAT_AGG_CONFIGURE
+#define EXPAT_AGG_CONFIGURE
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!! use this defines for buildin agg_svg_parser !!!!
+#define XML_STATIC
+#define COMPILED_FROM_DSP
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+#endif // EXPAT_AGG_CONFIGURE
